b460d4e9e443993bfd1ca34ddb95a0cf98c71a6b
Filin <nastia-dogf@mail.ru>
2022-07-27 16:33:15 +0300

    Added struct Person

---
+++ PhoneBook/Models/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  PhoneBook
+//
+//  Created by Anastasia Perminova on 27.07.2022.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: UInt
+    
+}
