ba5052b7c1ac55a5093ec94121171d9ef7c523e2
Filin <nastia-dogf@mail.ru>
2022-07-27 16:00:46 +0300

    Added TableView

--- PhoneBook/ViewControllers/PersonsListViewController.swift
+++ PhoneBook/ViewControllers/PersonsListViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class PersonsListViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
 
+}
+
+extension PersonsListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "text"
+        return cell
+    }
+    
+    
 }
 
